f161c475adce3ff1e0c2e75e0c90332ade177771
Marvelmobile <marvelmobiledh@gmail.com>
2020-03-10 18:48:38 -0300

    Acerto code Review

--- app/src/main/java/com/example/myapplication/view/FavoritosActivity.java
+++ app/src/main/java/com/example/myapplication/view/FavoritosActivity.java
@@ -10,7 +10,6 @@ public class FavoritosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favoritos);
-
     }
 
     @Override
